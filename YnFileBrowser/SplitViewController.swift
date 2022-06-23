cf78cdb8856f891441640f5d662d7caab76f577a
Jeffrey Sulton <notlus68@me.com>
2022-06-22 18:43:42 -0700

    Add support for changing detail views
    * Initialize the 3 detail view controllers
    * Initialize the `AppModel` instance
    * Add `handleSelectionChange()` function to switch between the
      view controllers

--- YnFileBrowser/SplitViewController.swift
+++ YnFileBrowser/SplitViewController.swift
@@ -1,17 +1,66 @@
-//
-//  SplitViewController.swift
-//  YnFileBrowser
-//
-//  Created by Jeffrey Sulton on 6/17/22.
-//
-
 import Cocoa
+import YnFileBrowserShared
+
+class DetailViewContainer: NSView {
+    override var acceptsFirstResponder: Bool { return true }
+}
 
 class SplitViewController: NSSplitViewController {
+    var viewModel: AppViewModel!
+    var imageViewController: ImageViewController!
+    var fileMetadataViewController: FileDetailsViewController!
+    var fileCollectionViewController: FileCollectionViewController!
+
+    var detailViewController: NSViewController {
+        splitViewItems[1].viewController
+    }
 
     override func viewDidLoad() {
+        viewModel = AppViewModel(splitViewController: self)
         super.viewDidLoad()
-        // Do view setup here.
+
+        imageViewController = storyboard!.instantiateController(
+            withIdentifier: "ImageViewController") as? ImageViewController
+
+        fileMetadataViewController = storyboard!.instantiateController(
+            withIdentifier: "MetadataViewController") as? FileDetailsViewController
+
+        fileCollectionViewController = storyboard!.instantiateController(
+            withIdentifier: "FileCollectionViewController") as? FileCollectionViewController
     }
-    
+
+    func handleSelectionChange(for fileNode: FileNode) {
+        if !detailViewController.children.isEmpty {
+            // Remove existing child
+            detailViewController.removeChild(at: 0)
+            detailViewController.view.subviews[0].removeFromSuperview()
+        }
+
+        if fileNode.isImage {
+            addChildToDetailViewController(imageViewController)
+            imageViewController.imageView.image = fileNode.getFileContents()
+        }
+        else if fileNode.isDirectory, !fileNode.children.isEmpty {
+            // Show the child files and folders
+            addChildToDetailViewController(fileCollectionViewController)
+            fileCollectionViewController.fileNodes = fileNode.children
+        }
+        else {
+            // Show file metadata
+            addChildToDetailViewController(fileMetadataViewController)
+            fileMetadataViewController.fileNode = fileNode
+        }
+    }
+
+    private func addChildToDetailViewController(_ childViewController: NSViewController) {
+        detailViewController.addChild(childViewController)
+        detailViewController.view.addSubview(childViewController.view)
+
+        NSLayoutConstraint.activate([
+            childViewController.view.topAnchor.constraint(equalTo: detailViewController.view.topAnchor),
+            childViewController.view.bottomAnchor.constraint(equalTo: detailViewController.view.bottomAnchor),
+            childViewController.view.leadingAnchor.constraint(equalTo: detailViewController.view.leadingAnchor),
+            childViewController.view.trailingAnchor.constraint(equalTo: detailViewController.view.trailingAnchor)
+        ])
+    }
 }
