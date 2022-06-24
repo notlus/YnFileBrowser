59ffd119bd0708af8cbab6ab4b44816e897d9ee5
Jeffrey Sulton <notlus68@me.com>
2022-06-24 11:33:56 -0700

    Check that parent process is launchd before starting helper

--- YnFileBrowserHelperTool/main.swift
+++ YnFileBrowserHelperTool/main.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+if getppid() != 1 {
+    NSLog("Yn File Browser helper can only be started by launchd")
+    exit(0)
+}
+
+NSLog("Starting Yn File Browser helper tool: PID \(getpid())")
+
 let fileBrowserService = FileBrowserService()
 
 let listener = NSXPCListener(machServiceName: "com.notlus.YnFileBrowser.Helper")
